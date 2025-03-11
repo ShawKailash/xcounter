63d3126d9ba075a52ca739f472e477c8674ef25b
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2025-03-11 14:00:28 +0000

    “xcounter”

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,7 +4,7 @@ function Counter (){
     const increment = () => setCount(count + 1);
     const decrement = () => setCount(count - 1);
     return (
-        <div className="name">
+        <div className="counter">
             <h1>Counter App</h1>
             <p>Count: {count}</p>
             <button onClick={increment}>Increment</button>
