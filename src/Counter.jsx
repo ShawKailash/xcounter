4aeadb23ac206d55efa9fb2b3ca9ef9ed761a865
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2025-03-11 13:54:37 +0000

    “xcounter”

---
+++ src/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function Counter (){
+    const [count, setCount] = useState(0);
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    return (
+        <div className="name">
+            <h1>Counter App</h1>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+export default Counter;
